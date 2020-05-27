895bb7db74185e896492abfbea9bd5956141448b
Mike Cohen <mjoecohen@gmail.com>
2020-05-27 07:59:00 +0100

    Split hackathon code out and added basic project administration map

--- contracts/maps.clar
+++ contracts/maps.clar
@@ -1,5 +1,47 @@
+;; Project Administration
+;; --------------------------
+
+;; Constants
+(define-constant administrator 'ST18PE05NG1YN7X6VX9SN40NZYP7B6NQY6C96ZFRC)
+(define-constant not-allowed (err 1))
+
+;; Storage
+(define-data-var counter int 0)
+
+;; PROJECT-MAP
+;; params: 
+;;     base-url - url from where to read project meta data
+;;     mint-fee - fee 
+(define-map project-map
+  ((project-id principal))
+  ((base-url (buff 40)) (mint-fee uint)))
+
+;; Add a new project - administrator level call.
+(define-public (add-project (projectId principal) (baseUrl (buff 40)) (mintFee uint))
+  (begin
+    (if (is-allowed)
+      (begin
+        (map-set project-map {project-id: projectId} ((base-url baseUrl) (mint-fee mintFee)))
+        (ok (var-get counter)))
+      (err not-allowed)
+    )
+  )
+)
+
+(define-read-only (get-project (projectId principal))
+  (unwrap-panic (map-get? project-map {project-id: projectId})))
+
+;; Only contract administrator can do these things.
+(define-private (is-allowed)
+  (is-eq tx-sender administrator)
+)
+
+;; Two Key Map Examples
+;; --------------------------
+;; 1. Storage
 (define-map two-key-map ((owner principal) (asset-hash (buff 32))) ((property1 (buff 40)) (property2 int)))
 
+;; 2. Public Functions
 ;; set values in map
 (define-public (set-value (asset (buff 32)) (name (buff 40)) (age int))
   (begin
@@ -9,15 +51,13 @@
   )
 )
 
-;; owner has been set via the transaction signature
+;; insert values in map
 (define-public (insert-value (asset (buff 32)) (name (buff 40)) (age int))
   (begin
   (ok (map-insert two-key-map (tuple (owner tx-sender) (asset-hash asset)) ((property1 name) (property2 age)))))
 )
 
-
-;; owner is passed in as an argument
+;; 3. Read Only Functions
+;; read values from a map
 (define-read-only (get-by-owner (asset (buff 32)) (owner principal))
   (unwrap-panic (map-get? two-key-map {owner: owner, asset-hash: asset})))
-
-;; owner is passed in as an argument
--- test/maps.ts
+++ test/maps.ts
@@ -1,18 +1,20 @@
 import { Client, Provider, ProviderRegistry, Result } from "@blockstack/clarity";
 import { assert } from "chai";
 import { readFromContract, execMethod } from "./query-utils"
+import * as fs from "fs";
 describe("maps tutorial test suite", () => {
 
+  const contractKeys = JSON.parse(fs.readFileSync("./keys-contract.json").toString());
+  const minterKeys = JSON.parse(fs.readFileSync("./keys-minter.json").toString());
   let client: Client;
   let provider: Provider;
 
-  const contractSig = "SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB";
-  const owner1Sig = "SP2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKNRV9EJ7";
-  const owner2Sig = "SP3T8WFCWHZNQ97SBYQH8T6ZJ1VWDMD46Y3VZ3JNJ";
+  const owner1Sig = contractKeys.stacksAddress;
+  const owner2Sig = minterKeys.stacksAddress;
 
   before(async () => {
     provider = await ProviderRegistry.createProvider();
-    client = new Client(contractSig + ".maps", "maps", provider);
+    client = new Client(owner1Sig + ".maps", "maps", provider);
   });
 
   describe("Deploying an instance of the contract", () => {
@@ -22,8 +24,18 @@ describe("maps tutorial test suite", () => {
     });
   });
 
-  describe("Inserting and getting from maps", () => {
-    it("inserting one object should let me get the object by key tuple", async () => {
+  describe("Project map tests", () => {
+    it("should allow new project if tx-sender is contract owner", async () => {
+      let txreceive = await execMethod(client, owner1Sig, "add-project", [ `'${owner2Sig}`, "\"http://risido.com/assets/v1\"","u5000"]);
+      assert.isOk(txreceive.success, "Added project");
+      const result = await readFromContract(client, "get-project", [`'${owner2Sig}`]);
+      assert.equal(result.strings[0], "http://risido.com/assets/v1");
+      assert.equal(result.strings.length, 1);
+    })
+  });
+/**
+  describe("Two key map operations", () => {
+    it("insert one object should allow retrieve the object by key", async () => {
       let txreceive = await execMethod(client, owner1Sig, "insert-value", ["\"hash1\"", "\"alice1\"", "1"]);
       assert.isOk(txreceive.success, "Inserted (alice1, 1)");
       const result = await readFromContract(client, "get-by-owner", ["\"hash1\"", `'${owner1Sig}`]);
@@ -44,12 +56,9 @@ describe("maps tutorial test suite", () => {
     })
     it("inserting same object twice returns false", async () => {
       let txreceive = await execMethod(client, owner1Sig, "insert-value", ["\"hash1\"", "\"alice1\"", "1"]);
-      assert.isOk(txreceive.result.indexOf('Transaction executed and committed. Returned: false') > -1), "Did not insert alice1");
+      assert.isOk(txreceive.result.indexOf('Transaction executed and committed. Returned: false') > -1, "Did not insert alice1");
       assert.isOk(txreceive.success, "Inserted alice1");
     })
-  });
-  
-  describe("Setting and getting from maps", () => {
     it("sets should return last entry", async () => {
       await execMethod(client, owner1Sig, "set-value", ["\"hash1\"", "\"bob1\"", "1"]);
       await execMethod(client, owner1Sig, "set-value", ["\"hash1\"", "\"bob2\"", "2"]);
@@ -68,10 +77,8 @@ describe("maps tutorial test suite", () => {
       assert.equal(result.strings.length, 1);
     })
   });
-
-  describe("Deletions from map ... ", () => {
-  });
-  after(async () => {
+**/
+after(async () => {
     await provider.close();
   });
 });
